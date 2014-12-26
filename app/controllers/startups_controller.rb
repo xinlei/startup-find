964fc6fa04e86afdaae92ed77799ea8f15902dcb
xinlei <wang.xinlei15@gmail.com>
2014-12-25 20:56:52 -0600

    add navigation, improved error handling

--- app/controllers/startups_controller.rb
+++ app/controllers/startups_controller.rb
@@ -2,71 +2,78 @@ require 'net/http'
 require 'json'
 
 class StartupsController < ApplicationController
+
+  # Display the search bar
   def index
     @data = {}
   end
 
+  # Return the fund raised by series for a specific company
+  # Return @data for the amount and @label for series type
   def show
-    query = params[:query]
-    result = {}
-
-    startup = Crunchbase::Organization.get(query)
-
-    if startup == nil
-      # Display error message
+    @query = params[:query]
+    user_key = params[:api_key]
+    if user_key == nil
+      flash[:notice] = "No api key provided"
     else
-      funding_rounds = get_funding_rounds(query)
-      for item in funding_rounds
-        path = item["path"]
-        funding_round = get_funding_round(path)
-        series = funding_round["properties"]["series"]
-        value = funding_round["properties"]["money_raised_usd"]
-        sum_by_series(series, value, result)
+      Crunchbase::API.key = user_key
+      result = {}
+      startup = Crunchbase::Organization.get(@query)
+      funding_rounds = get_funding_rounds(@query, user_key)
+      if startup == nil
+        flash[:notice] = "Company not found"
+      elsif funding_rounds == nil
+        flash[:notice] = "#{@query} has no funding history on Crunchbase"
+      else
+        flash[:notice] = nil
+        for item in funding_rounds
+          path = item["path"]
+          funding_round = get_funding_round(path, user_key)
+          series = funding_round["properties"]["series"]
+          value = funding_round["properties"]["money_raised_usd"]
+          sum_by_series(series, value, result)
+        end
+        result = result.sort
+        @data = []
+        @label = []
+        for n in result
+          @data << n[1]
+          @label << n[0]
+        end
       end
     end
-    puts result
-    result = result.sort
-    @query = query
-    @data = []
-    @label = []
-
-    for n in result
-      @data << n[1]
-      @label << n[0]
-    end
-
-    puts result
-    puts @data
   end
 
-  def get_funding_rounds(query)
-    user_key = ""
+  # Return the funding rounds for an organization
+  def get_funding_rounds(query, user_key)
     uri = URI("http://api.crunchbase.com/v/2/organization/#{query}/funding_rounds?user_key=#{user_key}")
     res = Net::HTTP.get_response(uri)
     ret = JSON.parse(res.body)
     ret["data"]["items"]
   end
 
-  def get_funding_round(path)
-    user_key = ""
+  # Return properties of the specific funding round
+  def get_funding_round(path, user_key)
     uri = URI("http://api.crunchbase.com/v/2/#{path}?user_key=#{user_key}")
     res = Net::HTTP.get_response(uri)
     ret = JSON.parse(res.body)
     ret["data"]
   end
 
+  # Sum the fund raised by series
   def sum_by_series(series, value, data)
     if series == nil
       series = "n/a"
     end
     series_symbol = series.to_s.capitalize
-    value = value/1000000
-    if data[series_symbol] == nil
-      data[series_symbol] = value
-    else
-      data[series_symbol] = data[series.to_s] + value
+    unless value == nil
+      value = value/1000000
+      if data[series_symbol] == nil
+        data[series_symbol] = value
+      else
+        data[series_symbol] = data[series_symbol] + value
+      end
     end
-
   end
 
 end
--- config/initializers/crunchbase.rb
+++ config/initializers/crunchbase.rb
@@ -1,4 +1,3 @@
 require 'crunchbase'
 
-Crunchbase::API.key = '895c34e4aff75534f72ee3bc55e4a516'
 Crunchbase::API.debug = false
